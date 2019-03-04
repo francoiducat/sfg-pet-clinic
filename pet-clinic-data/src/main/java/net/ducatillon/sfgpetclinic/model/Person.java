cf2d61777940a7202a8f6c7809d94b04c35c5d1a
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-03-04 13:20:50 +0100

    implement Base entity. Closes #13

--- pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/model/Person.java
@@ -3,7 +3,7 @@ package net.ducatillon.sfgpetclinic.model;
 import lombok.Data;
 
 @Data
-public class Person {
+public class Person extends BaseEntity{
 
   private String firstName;
   private String lastName;
