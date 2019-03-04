a13f8d0c7ca8a59715d9c9006e421723dfcfa285
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-03-04 13:09:28 +0100

    implement interfaces for pet clinic services Closes #5

---
+++ pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,14 @@
+package net.ducatillon.sfgpetclinic.services;
+
+import net.ducatillon.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+
+}
