a86873cf512e346c6c5d941d5c0637b2d382c3ed
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-03-21 14:03:52 +0100

    implement map based services. Closes #18

---
+++ pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package net.ducatillon.sfgpetclinic.services.map;
+
+import java.util.Set;
+import net.ducatillon.sfgpetclinic.model.Owner;
+import net.ducatillon.sfgpetclinic.services.CrudService;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+  @Override
+  public Set<Owner> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public Owner findById(Long id) {
+    return super.findById(id);
+  }
+
+  @Override
+  public Owner save(Owner object) {
+    return super.save(object.getId(), object);
+  }
+
+  @Override
+  public void delete(Owner object) {
+    super.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+}
---
+++ pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package net.ducatillon.sfgpetclinic.services.map;
+
+import java.util.Set;
+import net.ducatillon.sfgpetclinic.model.Vet;
+import net.ducatillon.sfgpetclinic.services.CrudService;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+  @Override
+  public Set<Vet> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public Vet findById(Long id) {
+    return super.findById(id);
+  }
+
+  @Override
+  public Vet save(Vet object) {
+    super.save(object.getId(), object);
+    return object;
+  }
+
+  @Override
+  public void delete(Vet object) {
+    super.delete(object);
+
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+}
