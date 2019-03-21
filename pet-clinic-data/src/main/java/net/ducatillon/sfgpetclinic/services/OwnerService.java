2118b2f3138a2820160ed67d7c837e2ba49e0430
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-03-21 13:20:37 +0100

    refactoring to common crud interface. Closes #16

--- pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/services/OwnerService.java
@@ -2,13 +2,9 @@ package net.ducatillon.sfgpetclinic.services;
 
 import net.ducatillon.sfgpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 
 }
