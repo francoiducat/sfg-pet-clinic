a32fb897e40e4ff2d5bb40e2a1afb4d81da823cf
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-03-28 21:13:19 +0100

    implement owners page and controller. Closes #21

---
+++ pet-clinic-web/src/main/java/net/ducatillon/sfgpetclinic/controller/OwnerController.java
@@ -0,0 +1,15 @@
+package net.ducatillon.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+  @RequestMapping({"", "/", "/index", "/index.html"})
+  public String getAllOwners() {
+
+    return "owners/index";
+  }
+}
