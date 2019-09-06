35c2f898547ca37ceb585fb2f2f6fdf743e80f03
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-09-06 16:49:45 +0200

    Add contact info to owners. Closes #43

--- pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/model/Owner.java
@@ -1,9 +1,14 @@
 package net.ducatillon.sfgpetclinic.model;
 
 import java.util.Set;
+import lombok.Data;
 
+@Data
 public class Owner extends Person {
 
+  private String address;
+  private String city;
+  private String phone;
   private Set<Pet> pets;
 
 }
