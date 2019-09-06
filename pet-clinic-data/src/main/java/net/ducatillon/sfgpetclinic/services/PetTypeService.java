237bbf192ce30bbd8293b6fc6f9f5230b075c741
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-09-06 17:20:26 +0200

    Add PetTypes to Boostrap. Closes #47

---
+++ pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package net.ducatillon.sfgpetclinic.services;
+
+import net.ducatillon.sfgpetclinic.model.PetType;
+
+/**
+ * Created by Francois on 09/06/2019.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
--- pet-clinic-web/src/main/java/net/ducatillon/sfgpetclinic/boostrap/DataLoader.java
+++ pet-clinic-web/src/main/java/net/ducatillon/sfgpetclinic/boostrap/DataLoader.java
@@ -1,8 +1,10 @@
 package net.ducatillon.sfgpetclinic.boostrap;
 
 import net.ducatillon.sfgpetclinic.model.Owner;
+import net.ducatillon.sfgpetclinic.model.PetType;
 import net.ducatillon.sfgpetclinic.model.Vet;
 import net.ducatillon.sfgpetclinic.services.OwnerService;
+import net.ducatillon.sfgpetclinic.services.PetTypeService;
 import net.ducatillon.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,14 +17,24 @@ public class DataLoader implements CommandLineRunner {
 
   private final OwnerService ownerService;
   private final VetService vetService;
+  private final PetTypeService petTypeService;
 
-  public DataLoader(OwnerService ownerService, VetService vetService) {
+  public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
     this.ownerService = ownerService;
     this.vetService = vetService;
+    this.petTypeService = petTypeService;
   }
 
   @Override
   public void run(String... args) throws Exception {
+
+    PetType dog = new PetType();
+    dog.setName("Dogy");
+    petTypeService.save(dog);
+    PetType cat = new PetType();
+    cat.setName("Datty");
+    petTypeService.save(cat);
+
     Owner owner1 = new Owner();
     owner1.setFirstName("Michael");
     owner1.setLastName("Weston");
