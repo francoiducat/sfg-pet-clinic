2cce52159eeefd0ae9eaa0656c85abe3db3c9f79
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-09-06 16:47:18 +0200

    Add specialities, add to vet. Closes #42

---
+++ pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,12 @@
+package net.ducatillon.sfgpetclinic.model;
+
+import lombok.Data;
+
+/**
+ * Created by Francois on 09/06/2019.
+ */
+@Data
+public class Speciality extends BaseEntity {
+
+  private String description;
+}
--- pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/model/Vet.java
@@ -1,4 +1,10 @@
 package net.ducatillon.sfgpetclinic.model;
 
+import java.util.Set;
+import lombok.Data;
+
+@Data
 public class Vet extends Person {
+
+  private Set<Speciality> specialities;
 }
