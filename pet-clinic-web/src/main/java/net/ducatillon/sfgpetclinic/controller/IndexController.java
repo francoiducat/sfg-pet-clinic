e6a9ced0a33308d472d785575c60cb08a392375b
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-09-18 15:22:02 +0200

    Fix broken menu links. Closes#60

--- pet-clinic-web/src/main/java/net/ducatillon/sfgpetclinic/controller/IndexController.java
+++ pet-clinic-web/src/main/java/net/ducatillon/sfgpetclinic/controller/IndexController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+  @RequestMapping("/oups")
+  public String oups() {
+    return "notimplemented";
+  }
+
   @RequestMapping({"", "/", "index", "index.html"})
   public String index() {
     return "index";
