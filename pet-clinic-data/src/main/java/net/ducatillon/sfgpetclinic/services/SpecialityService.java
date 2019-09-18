2a26636da6659a53b5bc44d00e380e9409b7e4cb
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-09-18 14:02:39 +0200

    Add Speciality Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/net/ducatillon/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package net.ducatillon.sfgpetclinic.services;
+
+import net.ducatillon.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by Francois on 09/18/2019.
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
